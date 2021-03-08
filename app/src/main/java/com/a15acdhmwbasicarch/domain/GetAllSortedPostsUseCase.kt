f874653821886aef05f14893072fd53df77b0687
Rodion <rodion.gramushniak@gmail.com>
2021-03-08 19:01:31 +0200

    add flow on in soting useCase

--- app/src/main/java/com/a15acdhmwbasicarch/domain/GetAllSortedPostsUseCase.kt
+++ app/src/main/java/com/a15acdhmwbasicarch/domain/GetAllSortedPostsUseCase.kt
@@ -2,17 +2,22 @@ package com.a15acdhmwbasicarch.domain
 
 import com.a15acdhmwbasicarch.data.PostsInfoRepository
 import com.a15acdhmwbasicarch.datasource.model.AddedFrom
+import com.a15acdhmwbasicarch.di.IoDispatcher
 import com.a15acdhmwbasicarch.domain.model.UserPostDomainModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetAllSortedPostsUseCase @Inject constructor(
-    private val postRepository: PostsInfoRepository
+    private val postRepository: PostsInfoRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
 
     operator fun invoke(): Flow<List<UserPostDomainModel>> =
-        postRepository.getPostsFromLocalStorage().map(::sort)
+        postRepository.getPostsFromLocalStorage().map(::sort).flowOn(ioDispatcher)
 
     private fun sort(startList: List<UserPostDomainModel>): List<UserPostDomainModel> {
         val postsGroupedByAddedFrom = startList.groupBy { it.addedFrom }.withDefault { emptyList() }
