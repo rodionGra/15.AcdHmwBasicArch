db733ba7ad4c7ffbde425f696c533ba19589e948
Rodion <rodion.gramushniak@gmail.com>
2021-03-12 15:58:37 +0200

    add view model test

--- app/src/test/java/com/a15acdhmwbasicarch/CoroutineTestRule.kt
+++ app/src/test/java/com/a15acdhmwbasicarch/CoroutineTestRule.kt
@@ -3,15 +3,17 @@ package com.a15acdhmwbasicarch
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class CoroutineTestRule(
+class MainCoroutineRule(
     private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
-) : TestWatcher() {
+) : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+
     override fun starting(description: Description?) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
@@ -19,7 +21,7 @@ class CoroutineTestRule(
 
     override fun finished(description: Description?) {
         super.finished(description)
+        cleanupTestCoroutines()
         Dispatchers.resetMain()
-        dispatcher.cleanupTestCoroutines()
     }
 }
--- app/src/test/java/com/a15acdhmwbasicarch/presentation/mainActivity/MainViewModelTest.kt
+++ app/src/test/java/com/a15acdhmwbasicarch/presentation/mainActivity/MainViewModelTest.kt
@@ -1,17 +1,35 @@
 package com.a15acdhmwbasicarch.presentation.mainActivity
 
-import com.a15acdhmwbasicarch.CoroutineTestRule
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.a15acdhmwbasicarch.MainCoroutineRule
+import com.a15acdhmwbasicarch.data.PostsInfoRepository
+import com.a15acdhmwbasicarch.tools.UpdatingState
+import io.kotlintest.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+@ExperimentalCoroutinesApi
 internal class MainViewModelTest{
 
     @get:Rule
-    val coroutineTestRule = CoroutineTestRule()
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineTestRule = MainCoroutineRule()
 
     @Test
     fun `update local storage`(){
-        
+        val mockKRepository = mockk<PostsInfoRepository>{
+            coEvery { updateLocalStorage() } returns Unit
+        }
+
+        val mainViewModel = MainViewModel(mockKRepository)
+
+        mainViewModel.updateRepo() shouldBe Unit
+
+        mainViewModel.errorLiveData.value shouldBe UpdatingState.COMPLETED
     }
 }
