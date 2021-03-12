364fbab85287a1d1217d2c8f7fc54cabce22912b
Rodion <rodion.gramushniak@gmail.com>
2021-03-12 14:53:26 +0200

    test for ci

---
+++ app/src/test/java/com/a15acdhmwbasicarch/presentation/mainActivity/MainViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.a15acdhmwbasicarch.presentation.mainActivity
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MainViewModelTest{
+
+    @Test
+    fun `update local storage`(){
+
+    }
+}
