a9d5ffaa3c4323e850bd60b77bb2119349ec49da
Rodion <rodion.gramushniak@gmail.com>
2021-03-12 15:04:03 +0200

    add coroutine test rule

--- app/src/test/java/com/a15acdhmwbasicarch/presentation/mainActivity/MainViewModelTest.kt
+++ app/src/test/java/com/a15acdhmwbasicarch/presentation/mainActivity/MainViewModelTest.kt
@@ -1,12 +1,17 @@
 package com.a15acdhmwbasicarch.presentation.mainActivity
 
+import com.a15acdhmwbasicarch.CoroutineTestRule
+import org.junit.Rule
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class MainViewModelTest{
 
+    @get:Rule
+    val coroutineTestRule = CoroutineTestRule()
+
     @Test
     fun `update local storage`(){
-
+        
     }
 }
